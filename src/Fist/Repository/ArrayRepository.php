2cabb94ced96f0083ccf9581add25924c7902d99
Mark Topper <mark@ulties.com>
2016-09-08 19:02:22 +0200

    Use separator

--- src/Fist/Repository/ArrayRepository.php
+++ src/Fist/Repository/ArrayRepository.php
@@ -6,14 +6,22 @@ class ArrayRepository implements RepositoryInterface
 {
     protected $items = [];
 
-    public function __construct(array $items = [])
+    protected $separator;
+
+    public function __construct(array $items = [], $separator = '.')
     {
         $this->items = $items;
+
+        $this->separator = $separator;
     }
 
     public function get($key, $default = null)
     {
         if ($this->has($key)) {
+            if (! is_null($this->separator)) {
+                return $this->getSeparated($key, $this->items);
+            }
+
             return $this->items[$key];
         }
 
@@ -22,11 +30,65 @@ class ArrayRepository implements RepositoryInterface
 
     public function set($key, $value)
     {
-        $this->items[$key] = $value;
+        if (! is_null($this->separator)) {
+            $this->setSeparated($key, $value, $this->items);
+        } else {
+            $this->items[$key] = $value;
+        }
     }
 
     public function has($key)
     {
+        if (! is_null($this->separator)) {
+            return $this->hasSeparated($key, $this->items);
+        }
+
         return isset($this->items[$key]);
+    }
+
+    protected function getSeparated($key, array &$items)
+    {
+        $parts = explode($this->separator, $key);
+        $part = array_shift($parts);
+
+        if (count($parts) == 0) {
+            return $items[$part];
+        }
+
+        return $this->getSeparated(
+            implode($this->separator, $parts),
+            $items[$part]
+        );
+    }
+
+    protected function setSeparated($key, $value, array &$items)
+    {
+        $parts = explode($this->separator, $key);
+        $part = array_shift($parts);
+
+        if (count($parts) == 0) {
+            $items[$part] = $value;
+        }
+
+        return $this->setSeparated(
+            implode($this->separator, $parts),
+            $value,
+            $items[$part]
+        );
+    }
+
+    protected function hasSeparated($key, array $items)
+    {
+        $parts = explode($this->separator, $key);
+        $part = array_shift($parts);
+
+        if (! isset($items[$part]) || count($parts) == 0) {
+            return isset($items[$part]);
+        }
+
+        return $this->hasSeparated(
+            implode($this->separator, $parts),
+            $items[$part]
+        );
     }
 }
