0a0a4d2b37c99c26c5a6a7c9fef521a9319975bc
Mark Topper <mark@ulties.com>
2016-09-14 19:04:25 +0200

    Add throwsException helper method

--- src/Fist/Testing/TestCase.php
+++ src/Fist/Testing/TestCase.php
@@ -2,6 +2,29 @@
 
 namespace Fist\Testing;
 
-class TestCase extends \PHPUnit_Framework_TestCase
+use Closure;
+use Exception;
+use PHPUnit_Framework_TestCase;
+use PHPUnit_Framework_AssertionFailedError;
+
+class TestCase extends PHPUnit_Framework_TestCase
 {
+    public function throwsException(Closure $closure, $class = null, $message = null)
+    {
+        try {
+            $closure();
+
+            $this->fail('Method did not throw exception as expected.');
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            throw $e;
+        } catch (Exception $e) {
+            if (!is_null($class)) {
+                $this->assertEquals($class, get_class($e));
+            }
+
+            if (!is_null($message)) {
+                $this->assertEquals($message, $e->getMessage());
+            }
+        }
+    }
 }
