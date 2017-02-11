7da535fca85a216770b57a3df640054efc729ed7
Mark Topper <mark@ulties.com>
2017-02-11 17:51:56 +0100

    Add shebangs and fix issue

--- build/split-full.sh
+++ build/split-full.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git subsplit init https://github.com/fistlab/php.git
 git subsplit publish --heads="master" src/Fist/Container:https://github.com/fistphp/container.git
 git subsplit publish --heads="master" src/Fist/Repository:https://github.com/fistphp/repository.git
